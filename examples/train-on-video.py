2cf44a613c20fdfd172d74b8070a33fac9fe45e0
vinyesm <vinyes.marina@gmail.com>
2024-04-12 08:10:00 +0000

    minimal working example of training with videos

--- examples/train-on-video.py
+++ examples/train-on-video.py
@@ -6,45 +6,76 @@ from torch.utils.data import Dataset, DataLoader, RandomSampler
 import webdataset as wds
 from random import random
 import numpy as np
+from PIL import Image
+
 
 
 # 1. Dataloader
 
-train_dataset_paths = "tiny-video-data.tar"
-valid_dataset_paths = "tiny-video-data.tar"
+train_dataset_paths = "phys101_frames.tar"
+valid_dataset_paths = "phys101_frames.tar"
 
-class RandomApply(nn.Module):
-    def __init__(self, prob, fn, fn_else = lambda x: x):
-        super().__init__()
-        self.fn = fn
-        self.fn_else = fn_else
-        self.prob = prob
-    def forward(self, x):
-        fn = self.fn if random() < self.prob else self.fn_else
-        return fn(x)
+# class RandomApply(nn.Module):
+#     def __init__(self, prob, fn, fn_else = lambda x: x):
+#         super().__init__()
+#         self.fn = fn
+#         self.fn_else = fn_else
+#         self.prob = prob
+#     def forward(self, x):
+#         fn = self.fn if random() < self.prob else self.fn_else
+#         return fn(x)
+
+# transform = transforms.Compose([
+#     transforms.Resize(32),
+#     RandomApply(0.1, transforms.RandomResizedCrop(32, scale=(0.5, 1.0), ratio=(0.98, 1.02)), transforms.CenterCrop(32)),
+#     transforms.ToTensor()
+# ])
 
 transform = transforms.Compose([
     transforms.Resize(32),
-    RandomApply(0.1, transforms.RandomResizedCrop(32, scale=(0.5, 1.0), ratio=(0.98, 1.02)), transforms.CenterCrop(32)),
+    transforms.CenterCrop(32),
     transforms.ToTensor()
 ])
 
+# TODO: handle batch size > 1
+batch_size = 1
+num_workers = 64
 
+def apply_transform(image):
+    # Convert the data type to uint8 if it's float32
+    if image.dtype == np.float32:
+        image = (image * 255).astype(np.uint8)
+    # Reshape the array if necessary (remove singleton dimensions)
+    if image.shape[-1] == 1:
+        image = np.squeeze(image, axis=-1)
+    pil_image = Image.fromarray(image)
+    # Apply transformation here
+    transformed_image = transform(pil_image)
+    return transformed_image
 
 def assemble_frames(sample):
-    print(sample.keys())
-    print(sample["__key__"])
-    images = np.array([sample[f"image{i:03d}.jpeg"] for i in range(1, 2)])
+    #print(sample.keys())
+    #print(sample["__key__"])
+    frames = [x for x in sample.keys() if x.split('.')[-1] == "jpeg"]
+    # TODO add padding and handle it in trainer (now all tensors need same size)
+    # this is a workaround for 
+    # RuntimeError: stack expects each tensor to be equal size, but got [71, 3, 32, 32] at entry 0 and [89, 3, 32, 32] at entry 1
+    frames = frames[:5]
+    images = np.array([apply_transform(sample[frame]) for frame in frames])
     return images
 
-ds = wds.WebDataset(train_dataset_paths).decode("rgb").map(assemble_frames)
+train_ds = wds.WebDataset(train_dataset_paths).decode("rgb").map(assemble_frames).shuffle(100)
+train_dl = wds.WebLoader(train_ds, batch_size=batch_size, num_workers=num_workers)
 
-for images in ds:
+valid_ds = wds.WebDataset(valid_dataset_paths).decode("rgb").map(assemble_frames).shuffle(100)
+valid_dl = wds.WebLoader(valid_ds, batch_size=batch_size, num_workers=num_workers)
+
+for images in train_ds:
     break
 
 print(images.shape)
 
-exit()
+# exit()
 
 # 2. model
 
@@ -80,7 +111,7 @@ trainer = VideoTokenizerTrainer(
     use_wandb_tracking = True,
     train_dataloader = train_dl,    
     valid_dataloader = valid_dl,
-    dataset_type = 'images',                        # 'videos' or 'images', prior papers have shown pretraining on images to be effective for video synthesis
+    dataset_type = 'videos',                        # 'videos' or 'images', prior papers have shown pretraining on images to be effective for video synthesis
     batch_size = batch_size,
     discr_start_after_step = 100,
     grad_accum_every = 1,
@@ -92,5 +123,7 @@ trainer = VideoTokenizerTrainer(
 # with trainer.trackers(project_name = 'magvit2', run_name = 'baseline'):
 import wandb
 
-wandb.init(project="magvit2")
+wandb.init(project="magvit2-video")
 trainer.train()
+
+
